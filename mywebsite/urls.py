a31fbaef0028c814850e6f63b913213f57bccbce
ARYAM <164507484+ProAryam-X100@users.noreply.github.com>
2025-04-15 11:05:35 +0300

    Update urls.py

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -25,3 +25,5 @@ path('contact/', views.contact_view, name='contact'),
 
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
