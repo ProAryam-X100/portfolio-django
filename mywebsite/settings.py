9d8f68d1fdce67f4642c9c9dcb18e500d5f77241
ARYAM <164507484+ProAryam-X100@users.noreply.github.com>
2025-04-15 08:53:38 +0300

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-lh5-2-aw^un-mx%=gsz9m3=e4dc9fdlfj$%ug@@ko_%*f+eb8)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['portfolio-django-vp7d.onrender.com']
 
 # Application definition
 
